3fd65b03ef2af3f1315c8d8a68eb9a9b50b4246d
James Smith <james.smith@djalbat.com>
2017-07-23 10:18:38 +0100

    Fixed the initialise() method of the file name draggable entry.

--- es6/explorer/draggableEntry/fileName.js
+++ es6/explorer/draggableEntry/fileName.js
@@ -71,6 +71,8 @@ class FileNameDraggableEntry extends DraggableEntry {
   }
   
   initialise(recognised, hidden)  {
+    super.initialise();
+    
     this.setHidden(hidden);
 
     this.setRecognised(recognised);
